711031274bacf34cad5205028ae282852beaf262
Claudio-P <pclaudio435@gmail.com>
2020-09-09 19:11:31 -0300

    Conexão com websocket estabelecida.

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const app = require('./config/server');
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log('Running...');
+});
+
+const io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+    console.log('Usuário conectado!');
+
+    socket.on('disconnect', (sokcet) => {
+        console.log('Usuário disconectado!');
+    });
 });
